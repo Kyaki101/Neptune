12ce74adab6db832d821cb2c77df49af32c0177f
Emmanuel <emanuelrs101@gmail.com>
2025-07-30 01:03:09 -0500

    Made some tests on vector2d

--- tests/linear_algebra_tests.cpp
+++ tests/linear_algebra_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../include/linear_algebra/vector2d.h"
+#include <stdexcept>
 
 TEST(LinearAlgebra, Basic) {
 
@@ -13,4 +14,23 @@ TEST(LinearAlgebra, DotProdTest) {
     linear_algebra::Vector2D vectorB(5.0, 8.0);
 
     EXPECT_EQ(vectorA.dotProduct(vectorB), 42.0);
+}
+
+TEST(LinearAlgebra, projectionTest) {
+    linear_algebra::Vector2D vectorA(4, 6);
+    linear_algebra::Vector2D vectorB(2, 0);
+    EXPECT_EQ(vectorA.projection(vectorB), linear_algebra::Vector2D(4.0, 0.0));
+}
+
+TEST(LinearAlgebra, NullVecProjection) {
+    linear_algebra::Vector2D vectorA(30.0, 43.0);
+    linear_algebra::Vector2D vectorB(0.0, 0.0);
+    ASSERT_THROW(vectorA.projection(vectorB), std::invalid_argument);
+}
+
+TEST(LinearAlgebra, VectorAddition) {
+    linear_algebra::Vector2D vectorA(24.0, 54.0);
+    linear_algebra::Vector2D vectorB(67.0, 43.0);
+    linear_algebra::Vector2D vectorC = vectorA + vectorB;
+    EXPECT_EQ(vectorC, linear_algebra::Vector2D(91.0, 97.0));
 }
